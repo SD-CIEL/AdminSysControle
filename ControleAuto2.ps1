58fc85a15045c81e1077b0a5c23ef38fbcb148f4
sergeP140 <serge.delbosc@outlooh.fr>
2025-02-20 11:15:20 +0100

    Ajout automatique de Posh-ssh si necessaire

--- ControleAuto2.ps1
+++ ControleAuto2.ps1
@@ -2,20 +2,59 @@
 # Pour sudo sans mot de passe :  "sudo visudo"  ajouter  "utilisateur ALL=(ALL) NOPASSWD: ALL"
 
 # Importer le module Posh-SSH
-Import-Module Posh-SSH
+
+# Créer le dossier s'il n'existe pas
+if (-not (Test-Path $ModulePath)) {
+    New-Item -ItemType Directory -Path $ModulePath -Force
+}
+
+# Ajouter le chemin aux modules PowerShell
+$env:PSModulePath = "$ModulePath;$env:PSModulePath"
+$FilteredPaths = $CurrentPaths | Where-Object {$_ -notmatch "OneDrive"}
+# Réappliquer les nouveaux chemins sans OneDrive
+$env:PSModulePath = $FilteredPaths -join ";"
+
+# Vérifier si le module Posh-SSH est installé
+if (-not (Get-Module -ListAvailable -Name Posh-SSH)) {
+    Write-Host "Le module Posh-SSH n'est pas installé. Installation en cours..." -ForegroundColor Yellow
+    
+    # Installer le module
+    try {
+        
+        #Install-Module -Name Posh-SSH -Force -Scope CurrentUser
+        Install-Module -Name Posh-SSH -Force -Scope CurrentUser -Verbose -ErrorAction Stop
+
+    
+        #Install-Module -Name Posh-SSH -Force -Scope CurrentUser -Repository PSGallery -SkipPublisherCheck -Des $ModulePath
+
+        Write-Host "Installation réussie !" -ForegroundColor Green
+    } catch {
+        Write-Host "Erreur lors de l'installation de Posh-SSH : $_" -ForegroundColor Red
+        exit 1
+    }
+} else {
+    Write-Host "Le module Posh-SSH est déjà installé." -ForegroundColor Green
+}
+
+# Importer le module dans la session actuelle
+Import-Module Posh-SSH -ErrorAction Stop
+Write-Host "Module Posh-SSH chargé avec succès." -ForegroundColor Cyan
+
+
+
 
 # Variables
-$remoteUser = "prof"
-$password = "frop"
+$remoteUser ="prof" #"utilisateur" #
+$password = "frop"#"didalab" #
 $privateKeyPath = "C:\Path\To\Your\PrivateKey"
 $PasswordSecur = ConvertTo-SecureString $password -AsPlainText -Force
 $Creds = New-Object System.Management.Automation.PSCredential ($remoteUser , $PasswordSecur)
 
-$timeout =30 # Timeout de connexion
+$timeout =40 # Timeout de connexion
 $intervalSeconds = 10  # Intervalle entre chaque itération en secondes
 
 #Liste des Machine à controler
-$remoteHosts = @("192.168.0.167","127.0.0.1")
+$remoteHosts = @("192.168.1.100","192.168.1.38","192.168.1.39")
 
 
 # Lire le fichier CSV des controles
