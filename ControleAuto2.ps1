5fc3d807988208f83fd115808415d6881b6280af
SD <serge.delbosc@outlook.fr>
2025-02-20 19:09:23 +0100

    Fichier csv pour les VM. Tableau unique pour les sessions et résultats

--- ControleAuto2.ps1
+++ ControleAuto2.ps1
@@ -1,7 +1,25 @@
-﻿#Script contrôle sur OS Linux Debian dans via SSH
-# Pour sudo sans mot de passe :  "sudo visudo"  ajouter  "utilisateur ALL=(ALL) NOPASSWD: ALL"
+﻿# Script contrôle sur OS Linux Debian dans via SSH
+# Pour sudo sans mot de passe :  "sudo visudo"  ajouter  "prof ALL=(ALL) NOPASSWD: ALL"
 
+# Variables
+$remoteUser ="prof" #"utilisateur" #
+$password = "frop"#"didalab" #
+$privateKeyPath = "C:\Path\To\Your\PrivateKey"
+$PasswordSecur = ConvertTo-SecureString $password -AsPlainText -Force
+$Creds = New-Object System.Management.Automation.PSCredential ($remoteUser , $PasswordSecur)
+
+$timeout =40 # Timeout de connexion
+$intervalSeconds = 10  # Intervalle entre chaque itération en secondes
+
+#Liste des Machine à controler
+$remoteHosts = @("192.168.1.100","192.168.1.38","192.168.1.39")
 # Importer le module Posh-SSH
+
+
+# Liste VM
+$VM = @()
+
+
 
 # Créer le dossier s'il n'existe pas
 if (-not (Test-Path $ModulePath)) {
@@ -43,18 +61,43 @@ Write-Host "Module Posh-SSH chargé avec succès." -ForegroundColor Cyan
 
 
 
-# Variables
-$remoteUser ="prof" #"utilisateur" #
-$password = "frop"#"didalab" #
-$privateKeyPath = "C:\Path\To\Your\PrivateKey"
-$PasswordSecur = ConvertTo-SecureString $password -AsPlainText -Force
-$Creds = New-Object System.Management.Automation.PSCredential ($remoteUser , $PasswordSecur)
 
-$timeout =40 # Timeout de connexion
-$intervalSeconds = 10  # Intervalle entre chaque itération en secondes
+# Lire le fichier CSV des machines et étudiants
+# Récupérer le dossier où se trouve le script
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$csvPath = Join-Path $scriptPath "IPEtudiants.csv"
 
-#Liste des Machine à controler
-$remoteHosts = @("192.168.1.100","192.168.1.38","192.168.1.39")
+# Vérifier si le fichier existe
+if (-Not (Test-Path $csvPath)) {
+    Write-Host "❌ Le fichier $csvPath n'existe pas dans le dossier du script !" -ForegroundColor Red
+    exit
+}
+else {
+    Write-Host "✅ Fichier $csvPath chargé" -ForegroundColor Green
+}
+
+# Lire le fichier CSV
+$data = Import-Csv $csvPath
+
+# Remplir les tableaux
+foreach ($row in $data) {
+    $VM+= @( New-Object PSObject -Property @{ ip = $row.iP; nom = $row.nomEtudiant } )
+}
+
+# Ajouter colonne Note
+foreach ($item in $VM) {
+   $item | Add-Member -MemberType NoteProperty -Name "NOTE" -Value "0"
+}
+# Ajouter colonne ping
+foreach ($item in $VM) {
+   $item | Add-Member -MemberType NoteProperty -Name "ping" -Value ""
+}
+# Ajouter colonne connect
+foreach ($item in $VM) {
+   $item | Add-Member -MemberType NoteProperty -Name "connect" -Value ""
+}
+
+
 
 
 # Lire le fichier CSV des controles
@@ -64,8 +107,11 @@ $csvPath = Join-Path $scriptPath "controle.csv"
 
 # Vérifier si le fichier existe
 if (-Not (Test-Path $csvPath)) {
-    Write-Host "❌ Le fichier data.csv n'existe pas dans le dossier du script !" -ForegroundColor Red
+    Write-Host "❌ Le fichier $csvPath n'existe pas dans le dossier du script !" -ForegroundColor Red
     exit
+}
+else {
+    Write-Host "✅ Fichier $csvPath chargé" -ForegroundColor Green
 }
 
 # Lire le fichier CSV
@@ -81,35 +127,88 @@ foreach ($row in $data) {
     $controleNames += $row.ControleName
     $commands += $row.Command
     $expectedValues += $row.Expected
+    foreach ($item in $VM) {
+        $item | Add-Member -MemberType NoteProperty -Name $row.ControleName -Value ""
+    }
+
 }
 
-#$controleNames = @("Test","sudo","hostName", "user1") 
-#$commands = @("uname",  "sudo whoami","hostname","id eleve")
-#$expectedValues = @( "Linux","root", "debian2016", "uid")
 
+
+
+#Test initial de connectivité PING
+$jobs = @()
+foreach ($item in $VM) {
+    $jobs += Start-Job -ScriptBlock {
+        param ($ip) Test-Connection -ComputerName $ip -Count 1 -Quiet
+    } -ArgumentList $item.iP
+}
+
+# Attente des résultats
+$jobs | Wait-Job
+$results = $jobs | Receive-Job
+
+# Mise à jour du tableau
+for ($i = 0; $i -lt $VM.Count; $i++) {
+    $VM[$i].ping = $results[$i]
+}
+
+# Nettoyage des jobs
+$jobs | Remove-Job
+
+
+$VM | Format-Table -AutoSize 
 
 
 #Ouverture des sessions
-# Liste dessessions
 Write-Host "Ouverture des sessions"
-$sessions = @()
-# Ouvrir toutes les sessions SSH en parallèle
-foreach ($remoteHost in $remoteHosts) {
-    $session = New-SSHSession -ComputerName $remoteHost -Credential $Creds -AcceptKey -ConnectionTimeout $timeout
-    if ($session.Connected) {
-        Write-Host "  ✅ Connexion réussie : $remoteHost" -ForegroundColor Green
-        $Sessions += $session
-    } else {
-        Write-Host "  ❌ Échec de connexion : $remoteHost" -ForegroundColor Red
-        $Sessions += [PSCustomObject]@{
-            Host     = $remoteHost
-            Connected = $false
+# Ouvrir toutes les sessions SSH 
+for ($i = 0; $i -lt $VM.Count; $i++) 
+ {
+    if ($VM[$i].ping)
+    {
+        $session = New-SSHSession -ComputerName $VM[$i].ip -Credential $Creds -AcceptKey -ConnectionTimeout $timeout
+        $VM[$i].connect=$session
+
+        if ($session.Connected) {
+            Write-Host "  ✅ Connexion réussie :" $VM[$i].ip -ForegroundColor Green
+            #$VM[$i].connect="True"
+        } else {
+            Write-Host "  ❌ Échec de connexion :" $VM[$i].ip -ForegroundColor Red
+            #$VM[$i].connect="False"
         }
     }
 }
 
 Write-Host "Fin ouverture sessions"
 Write-Host ""
+
+$VM | Format-Table -AutoSize 
+
+# Fonction pour tenter de joindre et connecté la VM 
+function Reconnect {
+    param (
+        [array]$vm, $i
+    )
+
+    if (Test-Connection -ComputerName $vm[$i].ip -Count 1 -Quiet) 
+    {
+       Write-Host "   ✅ " $vm[$i].ip "ping OK" -ForegroundColor Green
+       $vm[$i].ping=$true
+       $session = New-SSHSession -ComputerName $vm[$i].ip -Credential $Creds -AcceptKey -ConnectionTimeout $timeout
+       $VM[$i].connect=$session
+       if ($session.Connected) {
+            Write-Host "   ✅ " $vm[$i].ip "session établie"  -ForegroundColor Green
+       } else {
+           Write-Host "  ❌ " $vm[$i].ip "session échec de connexion !" -ForegroundColor Red
+       }
+    }
+    else
+    {
+        Write-Host "   ❌ " $ $vm[$i].ip "ping NOK !" -ForegroundColor Red
+        $vm[$i].ping=$false
+    }
+ }
 
 # Fonction pour exécuter les tests sur une session
 function Execute-Tests {
@@ -118,15 +217,9 @@ function Execute-Tests {
     )
         # Vérifier si la session est valide
     if (-not $session -or -not $session.Connected) {
-        Write-Host "  ❌ Session SSH invalide ou non connectée." -ForegroundColor Red
+        Write-Host "   ❌ Session SSH invalide ou non connectée." -ForegroundColor Red
         return $null
     }
-
-    # ouverture de la session
-    #$PasswordSecur = ConvertTo-SecureString "didalab" -AsPlainText -Force
-    #$Creds = New-Object System.Management.Automation.PSCredential ("utilisateur", $PasswordSecur)
-
-    #$Session = New-SSHSession -ComputerName $remoteHost -Credential $Creds -AcceptKey -ConnectionTimeout 60
 
     # Tableau pour stocker les résultats
     $results = @()
@@ -145,49 +238,67 @@ function Execute-Tests {
             $res=$result.Output
             Write-Host "  - Executing tests on host: $res" -ForegroundColor Green
     }
-
     return $results
 }
 
 # Boucle pour répéter les tests périodiquement
 while ($true) {
-    # Tableau pour stocker les résultats finaux
-    $finalResults = @()
 
-   foreach ($session in $Sessions) {
-    Write-Host "Executing tests on host: $($session.Host)"
+   for ($i = 0; $i -lt $VM.Count; $i++) 
+   {
+     if ($VM[$i].ping)
+     {
+       if ($VM[$i].connect.Connected)
+        {
 
-    $testResults = Execute-Tests -session $session
+        Write-Host "Executing tests on host: $($VM[$i].connect.Host)"
 
-    # Initialisation de l'objet résultat
-    $resultObject = [Ordered]@{
-        Host = $($session.Host)
+        $testResults = Execute-Tests -session $VM[$i].connect
+
+        # Réaliser les tests 
+        $note=0;
+
+        for ($j = 0; $j -lt $commands.Length; $j++) {
+            $command = $commands[$j]
+            $controleName = $controleNames[$j]
+            $columnName = "$controleName"
+
+            # Récupérer le résultat correspondant
+            $matchValue = ($testResults | Where-Object { $_.Command -eq $command } | Select-Object -First 1).Match
+
+            $VM[$i].$columnName=$matchValue
+            if ($matchValue) {$note++}
+        }  
+
+        $VM[$i].NOTE=$note
+        
+
+
+
+      }
+      else
+      {
+            Reconnect -vm $VM -i $i
+      }
+    }
+    else
+    {
+            Reconnect -vm $VM -i $i
     }
 
-    # Ajouter dynamiquement les colonnes Command1Match, Command2Match, etc.
-    for ($i = 0; $i -lt $commands.Length; $i++) {
-        $command = $commands[$i]
-        $controleName = $controleNames[$i]
-        $columnName = "$controleName"  # Génère "Command0Match", "Command1Match", etc.
-
-        # Récupérer le résultat correspondant
-        $matchValue = ($testResults | Where-Object { $_.Command -eq $command } | Select-Object -First 1).Match
-
-        # Ajouter dynamiquement la colonne
-        $resultObject[$columnName] = $matchValue
-    }
-
-    # Ajouter l'objet au tableau final
-    $finalResults += [PSCustomObject]$resultObject
 }
 
-    
-
     # Afficher les résultats sous forme de tableau
-    $finalResults | Format-Table -AutoSize 
+    clear
+    $VM | Format-Table -AutoSize 
+
+
 
     Start-Sleep -Seconds $intervalSeconds
 }
 
 # Fermer toutes les sessions SSH
 $sshSessions | ForEach-Object { Remove-SSHSession -SSHSession $_ }
+
+
+
